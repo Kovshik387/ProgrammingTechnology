e9ce01ff8aabc531aaad9aaee52bc10e3f38460e
Kovshik387 <fantokin03@mail.ru>
2023-10-07 18:15:30 +0300

    Add Rectangle

--- Task_2/src/main/java/Model/Rectangle.java
+++ Task_2/src/main/java/Model/Rectangle.java
@@ -18,7 +18,7 @@ public class Rectangle extends Shape {
     @Override
     public void drawShape(GraphicsContext context) {
         context.setFill(super.getColor());
-        context.fillPolygon(new double[]{super.getX(),getX()+ length*Math.tan(angle), getX()+ -length},new double[]{super.getY(),getY() -length,getY() -(length*Math.tan(angle))},3);
+        context.fillPolygon(new double[]{super.getX(),getX()+ length*Math.tan(angle), getX() -length},new double[]{super.getY(),getY() -length,getY() -(length*Math.tan(angle))},3);
     }
 
     @Override
