fefbc9dc6638a4bde4b1e3e02eca6913012e686b
Kovshik387 <fantokin03@mail.ru>
2023-12-11 02:21:30 +0300

    feat: add message

---
+++ src/main/java/com/example/prog/Application.java
@@ -0,0 +1,9 @@
+package com.example.prog;
+
+public class Application{
+    public static void main(String[] args) {
+        for (int i = 0; i < 15; i++){
+            System.out.println("01");
+        }
+    }
+}
