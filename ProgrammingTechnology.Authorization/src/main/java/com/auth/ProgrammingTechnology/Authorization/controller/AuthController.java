66b92d58d784e526ed24e278f98c90ff96a61dca
Kovshik387 <fantokin03@mail.ru>
2024-04-04 01:38:18 +0300

    git init

--- ProgrammingTechnology.Authorization/src/main/java/com/auth/ProgrammingTechnology/Authorization/controller/AuthController.java
+++ ProgrammingTechnology.Authorization/src/main/java/com/auth/ProgrammingTechnology/Authorization/controller/AuthController.java
@@ -1,16 +1,43 @@
 package com.auth.ProgrammingTechnology.Authorization.controller;
 
+import com.auth.ProgrammingTechnology.Authorization.dal.model.Account;
+import com.auth.ProgrammingTechnology.Authorization.dal.model.request.SignInRequest;
+import com.auth.ProgrammingTechnology.Authorization.dal.model.request.SignUpRequest;
+import com.auth.ProgrammingTechnology.Authorization.dal.model.response.SignInResponse;
+import com.auth.ProgrammingTechnology.Authorization.dal.model.response.SignUpResponse;
+import com.auth.ProgrammingTechnology.Authorization.dal.repository.AccountDao;
+import com.auth.ProgrammingTechnology.Authorization.services.AuthorizationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLException;
+import java.util.Collection;
 
 @RestController
-@RequestMapping("api/auth")
+@RequiredArgsConstructor
+@RequestMapping("api/")
 public class AuthController {
+    @Autowired
+    private final AuthorizationService authorizationService;
     @PostMapping("SignUp")
-    public ResponseEntity<String> SignUp(){ return null;}
+    public ResponseEntity<SignUpResponse> signUp(SignUpRequest request) throws SQLException {
+        return ResponseEntity.ok(authorizationService.signUp(request));
+    }
     @GetMapping("SignIn")
-    public ResponseEntity<String> SignIn(){return null;}
+    public ResponseEntity<SignInResponse> signIn(SignInRequest request) throws Exception {
+        return ResponseEntity.ok(authorizationService.signIn(request));
+    }
+    @PostMapping
+    public ResponseEntity<String> getNewRefreshToken(@RequestBody String request){
+        return ResponseEntity.ok("");
+    }
+
+    @GetMapping("GetInfo")
+    public ResponseEntity<String> getInfo(){
+        return ResponseEntity.ok("Авторизирован");
+    }
 }
--- ProgrammingTechnology.Authorization/src/main/java/com/auth/ProgrammingTechnology/Authorization/dal/model/Account.java
+++ ProgrammingTechnology.Authorization/src/main/java/com/auth/ProgrammingTechnology/Authorization/dal/model/Account.java
@@ -1,29 +1,27 @@
 package com.auth.ProgrammingTechnology.Authorization.dal.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-@Entity
-@Table(name = "accounts")
+//@Entity
+//@Table(name = "accounts")
+@Builder
 public class Account {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "account_id")
-    public long id;
-    @Column(name = "name")
-    public String name;
-    @Column(name = "email")
-    public String email;
-    @Column(name = "password_hash")
-    public String passwordHash;
-    @Column(name = "refresh_token")
-    public String refreshToken;
-    @Column(name = "role_type")
-    public String roleType;
+//    @Column(name = "account_id")
+    private int id;
+//    @Column(name = "name")
+    private String name;
+//    @Column(name = "email")
+    private String email;
+//    @Column(name = "password_hash")
+    private String passwordHash;
+//    @Column(name = "refresh_token")
+    private String refreshToken;
+//    @Column(name = "role_type")
+    private String roleType;
 }
